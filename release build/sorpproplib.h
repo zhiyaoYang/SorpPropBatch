5af6d8f387d11883588c5200b510e0a4fc05eeae
Zhiyao Yang <snowyang1111@gmail.com>
2017-07-19 21:54:56 -0400

    update release build with necessary libraries

---
+++ release build/sorpproplib.h
@@ -0,0 +1,18 @@
+#ifndef SORPPROPLIB_H
+#define SORPPROPLIB_H
+
+#include "sorpproplib_global.h"
+
+#include <iostream>
+#include <map>
+
+class SORPPROPLIBSHARED_EXPORT SorpPropLib
+{
+
+public:
+    SorpPropLib();
+
+    void calc(std::string ref, std::string sorb, double tK, double xMass);
+};
+
+#endif // SORPPROPLIB_H
